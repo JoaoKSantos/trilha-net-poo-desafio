b65dfe23a2b607fca30c74f25e1ad0974693d093
JoaoKSantos <joaoklebercsantos@gmail.com>
2025-07-30 14:03:45 -0300

    Implementação das propriedades faltantes e construtores, bem junto aos testes na class Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("981034505", "Nokia", "789879987987987", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("996906025", "Iphone", "12345678912345", 60);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("ICloud");
